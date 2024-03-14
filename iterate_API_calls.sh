1ba462e0d7b55eb8b1e66df843abd47adde165e6
andreaboscutti <114694254+andreaboscutti@users.noreply.github.com>
2024-03-13 22:37:42 -0500

    variables added

--- iterate_API_calls.sh
+++ iterate_API_calls.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 #Path to a .csv file (comma separated) with the following fields: "mriid,gender,age,brain_vol,ventricular_vol" (order matters)
-CSV_FILE="/home/aboscutti/diffusion/ALL_scaled.csv"
+CSV_FILE="/path/to/csv"
+
+#other variables
+API_KEY="api_key"
+PORT="8000"
+ADDRESS="http://000.000.0.000"
 
 # Read the .csv line by line and extract fields (order matters)
 tail -n +2 "$CSV_FILE" | while IFS=, read -r mriid gender age brain_vol ventricular_vol; do
   # API call
   curl -X 'POST' \
-    'http://184.105.4.118:8000/generategenderageventbrainvol' \
+    "$ADDRESS/generategenderageventbrainvol" \
     -H 'accept: application/json' \
-    -H 'Authorization: Bearer e3b76d48a3082e5e650019f5951f9a10' \
+    -H "Authorization: Bearer $API_KEY" \
     -H 'Content-Type: application/json' \
     -d "{
       \"mriid\": \"$mriid\",
